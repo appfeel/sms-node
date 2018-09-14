415867303d44e874cf3ecad9f9882775975ee354
Miquel Martín <miqmago@gmail.com>
2018-09-14 12:16:51 +0200

    fix tests

--- test/settings.js
+++ test/settings.js
@@ -1,4 +1,22 @@
-import { settings as gsettings, settings2 as gsettings2 } from './settings-gateway';
+/* eslint-disable global-require, padded-blocks */
+
+import fs from 'fs';
+
+let gsettings = {
+    user: '',
+    password: '',
+    url: '',
+};
+let gsettings2 = {
+    user: '',
+    password: '',
+};
+
+try {
+    const upsettings = JSON.parse(fs.readFileSync('./settings-gateway.json'));
+    gsettings = upsettings.settings;
+    gsettings2 = upsettings.settings2;
+} catch (e) {} // eslint-disable-line no-empty
 
 export const settings = {
     user: gsettings.user,
